02e2be3e6c26004de089581e769e714f4cd9d8bc
abshar-niduvat <17gcs29@meaec.edu.in>
2020-10-14 13:11:47 +0530

    Create positive_or_negative.py

---
+++ python/positive_or_negative.py
@@ -0,0 +1,10 @@
+# User enters the number
+number = int(input("Enter number: "))
+
+# checking the number
+if number < 0: print("The entered number is negative.") elif number > 0:
+    print("The entered number is positive.")
+elif number == 0:
+    print("Number is zero.")
+else:
+    print("The input is not a number")
