5352c55e54eeca7be5e8af2d04eb5b41d393d988
Am963 <66202734+Am963@users.noreply.github.com>
2020-10-14 10:22:57 +0530

    Create convert kilometers to miles.js

---
+++ JavaScript/convert kilometers to miles.js
@@ -0,0 +1,10 @@
+// taking kilometers input from the user
+let kilometers = prompt("Enter value in kilometers: ")
+
+// conversion factor
+let factor = 0.621371
+
+// calculate miles
+let miles = kilometers * factor
+
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
