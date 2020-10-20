ec0488b980c18f31255d35ad04b06460023fd022
William Welch <wwelch@ctcd.edu>
2020-10-20 12:55:50 -0500

    Create encrypt.py
    Encrypts a string of lowercase letters and prints
    the result.  The other input is the offset value.

---
+++ python/encrypt.py
@@ -0,0 +1,29 @@
+"""
+File: encrypt.py
+Encrypts a string of lowercase letters and prints
+the result.  The other input is the offset value.
+"""
+
+def encrypt(clear_text, distance):
+	code = ""
+	for ch in clear_text:
+		ordValue = ord(ch)
+		cipherValue = ordValue + distance
+		if cipherValue > ord('z'):
+			cipherValue = ord('a') + distance - \
+						  (ord('z') - ordValue + 1)
+		code +=  chr(cipherValue)
+	return (code)
+	
+
+def main():
+	plainText =input("Enter a one-word, lowercase message: ")
+	offset = int(input("Enter the distance value: "))
+	
+	result = encrypt(plainText, offset)
+	print("Cleartext: {:5s}. Offset:  {:2d} Ciphertext: {:5s}".format(plainText, offset, result))
+	
+	
+	
+
+main()
