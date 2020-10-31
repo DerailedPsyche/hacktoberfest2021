fb4522e9388f473567afc19c1cb7f4b1e1078640
Intaladear <chetta.busayarat@itorama.com>
2020-11-01 01:39:31 +0700

    Create square.js
    Create square.js

---
+++ square.js
@@ -0,0 +1,21 @@
+function setup() {  
+       
+    // Create Canvas of given size  
+    createCanvas(300, 300);  
+   
+}  
+   
+function draw() {  
+       
+    background(220); 
+       
+    // Use color() function 
+    let c = color('green'); 
+   
+    // Use fill() function to fill color 
+    fill(c); 
+     
+    // Draw a square 
+    square(50, 50, 200); 
+     
+}  
