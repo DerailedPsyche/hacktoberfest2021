2a9d808013ccecaa9d8fcb7635eec271eaac7ac0
me1006mea <73740655+me1006mea@users.noreply.github.com>
2020-10-31 15:53:51 +0530

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,9 @@
+// store input numbers
+let num1 = prompt('Enter the first number ');
+let num2 = prompt('Enter the second number ');
+
+//add two numbers
+let sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
